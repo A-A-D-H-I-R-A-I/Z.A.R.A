ba21d3eb249aece59e4cc9e7750895b3cacc1a8f
Mani <maniraj.paa@gmail.com>
2025-02-10 12:22:47 +0000

    ADDED: Password Manager API Template

---
+++ Zara.Api/Controllers/PasswordManagerController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Zara.Api;
+
+[ApiController]
+[Route("api/password-manager")]
+public class PasswordManagerController : ControllerBase
+{
+	#region Publics
+	[HttpGet]
+	public IActionResult GetAllPasswords()
+	{
+		return Ok();
+	}
+
+	[HttpGet("{application}")]
+	public IActionResult GetByApplication(string application)
+	{
+		return Ok();
+	}
+
+	[HttpPost]
+	public IActionResult AddPassword()
+	{
+		return CreatedAtAction(nameof(GetByApplication), new { application = string.Empty }, null);
+	}
+
+	[HttpPut]
+	public IActionResult UpdatePassword()
+	{
+		return NoContent();
+	}
+
+	[HttpDelete]
+	public IActionResult DeletePassword()
+	{
+		return NoContent();
+	}
+	#endregion
+}
