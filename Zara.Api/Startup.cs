bd901dae8d1b15a24d3b4d43a8f9c3728ad8928b
Mani <maniraj.paa@gmail.com>
2025-02-10 14:07:39 +0000

    ADDED: Zara data context and entities

--- Zara.Api/Startup.cs
+++ Zara.Api/Startup.cs
@@ -1,29 +1,41 @@
-﻿namespace Zara.Api;
+﻿using Microsoft.EntityFrameworkCore;
+using Zara.Data;
+
+namespace Zara.Api;
 
 internal static class Startup
 {
-    #region Internals
-    internal static void AddServices(this IServiceCollection services)
-    {
-        services.AddControllers();
+	#region Internals
+	internal static void AddServices(this IServiceCollection services, IConfiguration configuration)
+	{
+		services.AddDatabaseContext(configuration);
 
-        services.AddEndpointsApiExplorer();
-        
-        services.AddSwagger();
-    }
+		services.AddControllers();
 
-    internal static void UseServices(this IApplicationBuilder builder)
-    {
-        builder.ConfigureSwagger();
+		services.AddEndpointsApiExplorer();
 
-        builder.UseHttpsRedirection();
-        
-        builder.UseAuthorization();
-    }
+		services.AddSwagger();
+	}
 
-    internal static void ConfigureEndpoints(this IEndpointRouteBuilder routes)
-    {
-        routes.MapControllers();
-    }
-    #endregion
+	internal static void UseServices(this IApplicationBuilder builder)
+	{
+		builder.ConfigureSwagger();
+
+		builder.UseHttpsRedirection();
+
+		builder.UseAuthorization();
+	}
+
+	internal static void ConfigureEndpoints(this IEndpointRouteBuilder routes)
+	{
+		routes.MapControllers();
+	}
+	#endregion
+
+	#region Privates
+	private static void AddDatabaseContext(this IServiceCollection services, IConfiguration configuration)
+	{
+		services.AddDbContext<ZaraDataContext>(options => options.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
+	}
+	#endregion
 }
---
+++ Zara.Data/Entities/PasswordEntry.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Zara.Data;
+
+public class PasswordEntry
+{
+	#region Properties
+	public int Id { get; set; }
+
+	[MaxLength(100)]
+	public string Application { get; set; } = string.Empty;
+	[MaxLength(100)]
+	public string UserName { get; set; } = string.Empty;
+	[MaxLength(500)]
+	public string Password { get; set; } = string.Empty;
+	
+	public DateTime CreatedAt { get; set; }
+	public DateTime LastModifiedAt { get; set; }
+	#endregion
+}
